a4eefbda133090809fee26b016a33d44a7bce2e4
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-07-29 13:47:31 -0700

    Solved Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(workingArray, ...elements) {
+    for (const element of elements) {
+        for (let i = 0; i < workingArray.length; i++) {
+            if (workingArray[i] === element) {
+                workingArray.splice(i,1);
+            }
+        }
+    }
 
+    return workingArray;
 };
 
 // Do not edit below this line
