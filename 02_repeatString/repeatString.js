b03c235718df22b3a0621e174ad23a9be2e88472
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-07-29 11:54:30 -0700

    Solved Exercises 01 & 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num > 0) {
+        let repeatedString = string;
 
+        for (let i = 1; i < num; i++) {
+            repeatedString = repeatedString + string;
+        }
+
+        return repeatedString;
+    }
+    
+    else if (num == 0) {
+        return '';
+    }
+    
+    else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
