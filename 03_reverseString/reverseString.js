c6d053c903f76848e17480dcc2f0c42e143a403d
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-07-29 12:03:17 -0700

    Solved exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+    let reversedSentence = "";
+    
+    for (let i = sentence.length; i >= 0; i--) {
+        reversedSentence += sentence.charAt(i);
+    }
 
+    return reversedSentence;
 };
 
 // Do not edit below this line
