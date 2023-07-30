984da2f7c6c5757469425eece95097799a596475
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-07-30 16:02:28 -0700

    Solved Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    let sum = 0;
+
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber) ||
+        firstNumber < 0 || lastNumber < 0) {
+            return "ERROR";
+        }
+
+    else if (firstNumber > lastNumber) {
+        for (let i = lastNumber; i <= firstNumber; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    else {
+        for (let i = firstNumber; i <= lastNumber; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
 
 };
 
